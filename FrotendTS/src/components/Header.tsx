3d6e0d34d88e0470efa8fadf7bdc54d1e58d6ffa
Diego.Hernandez <diego.hernandez@ENCEDCCUULT0753.local>
2025-01-28 12:55:58 -0600

    Improved folder structure

---
+++ FrotendTS/src/components/Header.tsx
@@ -0,0 +1,7 @@
+const Header: React.FC = () => {
+    return (
+        <div> <h1>To Dos Application 📗</h1></div>
+    );
+};
+
+export default Header;
---
+++ backend/ToDosBackend/src/main/java/com/encora/ToDosBackend/service/ValidationException.java
@@ -0,0 +1,4 @@
+package com.encora.ToDosBackend.service;
+
+public class ValidationException {
+}
